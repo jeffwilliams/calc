a54a56cec9850022aaf90062799fec692d11d848
Jeff Williams <jeffsmessages@gmail.com>
2018-03-26 04:24:10 -0400

    Add function `now` to get time

--- builtin.go
+++ builtin.go
@@ -1,6 +1,9 @@
 package main
 
-import "math/big"
+import (
+	"math/big"
+	"time"
+)
 
 // builtin funcs
 func binom(n, k *big.Int) (*big.Int, error) {
@@ -12,8 +15,14 @@ func bit(n, i *big.Int) (*big.Int, error) {
 	return big.NewInt(int64(n.Bit(int(i.Int64())))), nil
 }
 
+func now() (*big.Int, error) {
+	t := time.Now()
+	return big.NewInt(int64(time.Duration(t.UnixNano()) / time.Millisecond)), nil
+}
+
 func init() {
 	RegisterBuiltin("binom", binom)
 	RegisterBuiltin("choose", binom)
 	RegisterBuiltin("bit", bit)
+	RegisterBuiltin("now", now)
 }
