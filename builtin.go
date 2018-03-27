115f4aa5e2b3fb319af1580a8242133d4c6a504d
Jeff Williams <jeffsmessages@gmail.com>
2018-03-27 01:15:14 -0400

    Added dice rolling func

--- builtin.go
+++ builtin.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"math/big"
+	"math/rand"
 	"time"
 )
 
@@ -20,9 +21,20 @@ func now() (*big.Int, error) {
 	return big.NewInt(int64(time.Duration(t.UnixNano()) / time.Millisecond)), nil
 }
 
+func roll(num, sides *big.Int) (*big.Int, error) {
+	sum := int64(0)
+	sd := sides.Int64()
+	for i := int64(0); i < num.Int64(); i++ {
+		sum += rand.Int63n(sd) + 1
+	}
+	return big.NewInt(sum), nil
+}
+
 func init() {
+	rand.Seed(time.Now().UnixNano())
 	RegisterBuiltin("binom", binom, "binmomial coeffient of (p1, p2)")
 	RegisterBuiltin("choose", binom, "p1 choose p2. Same as binom")
 	RegisterBuiltin("bit", bit, "return the value of bit p1, counting from 0")
 	RegisterBuiltin("now", now, "return the number of milliseconds since epoch")
+	RegisterBuiltin("roll", roll, "roll p1 dice each having p2 sides and sum the outcomes")
 }
